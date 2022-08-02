02aedb622bba1057b43d5ee4914ba0329ff94f80
Fernando Augusto López Plascencia <fa_lopez1@yahoo.com.mx>
2022-08-02 10:33:39 +0900

    Primer upload

---
+++ paso-07/script.js
@@ -0,0 +1,37 @@
+const init = () => {
+    console.log("El sitio acaba de cargar");
+
+    // obtener la referencia a un elemento HTML
+    const datosElem = document.querySelector("#monedas");
+    
+    // Creando un objeto que representa la solicitud HTTP que haremos después
+    const req = new Request("https://api.coingecko.com/api/v3/simple/price?ids=usd&vs_currencies=eth");
+
+    // Haciendo la solicitud. El valor de retorno es una promesa.
+    const promesa = fetch(req)
+    const respuestaJson = promesa.then((response) => {
+        // Cuando lleguen los datos de "promesa"...
+
+        // Si la respuesta contiene un error, decir que hay un error y terminar
+        if (!response.ok) {
+            throw new Error("Error al cargar datos");
+        }
+
+        // Si no hay error, procesar los datos de respuesta como un objeto JSON.
+        return response.json();
+    })
+    respuestaJson.then((respData) => {
+        // Luego, obtener los datos JSON de la función anterior y desplegarlos en la consola por ahora.
+        console.log(respData);
+
+        const equivalencia = respData.usd;
+        const valor = equivalencia.eth;
+
+        const datoEnHtml = `
+            <article class="respuesta">
+                1 usd = ${valor} eth
+            </article>`;
+        
+        datosElem.insertAdjacentHTML('beforeend', datoEnHtml);
+    });
+}
---
+++ paso-09/script.js
@@ -0,0 +1,63 @@
+const monedaOriginal = 'usd';
+let monedaEquivalencia = 'eth';
+
+const init = () => {
+    console.log("El sitio acaba de cargar");
+
+    // agregar el manejador de evento a la forma
+    const forma = document.querySelector('form');
+    forma.addEventListener('submit', evento => {
+        submitForm(evento);
+    });
+
+    // obtener la referencia a un elemento HTML
+    const datosElem = document.querySelector("#monedas");
+    
+    // Creando un objeto que representa la solicitud HTTP que haremos después
+    const req = new Request(`https://api.coingecko.com/api/v3/simple/price?ids=${monedaOriginal}&vs_currencies=${monedaEquivalencia}`);
+
+    // Haciendo la solicitud. El valor de retorno es una promesa.
+    const promesa = fetch(req)
+    const respuestaJson = promesa.then((response) => {
+        // Cuando lleguen los datos de "promesa"...
+
+        // Si la respuesta contiene un error, decir que hay un error y terminar
+        if (!response.ok) {
+            throw new Error("Error al cargar datos");
+        }
+
+        // Si no hay error, procesar los datos de respuesta como un objeto JSON.
+        return response.json();
+    })
+    respuestaJson.then((respData) => {
+        // Luego, obtener los datos JSON de la función anterior y desplegarlos en la consola por ahora.
+        console.log(respData);
+
+        const equivalencia = respData[monedaOriginal];
+        const valor = equivalencia[monedaEquivalencia];
+
+        const datoEnHtml = `
+            <article class="respuesta">
+                1 usd = ${valor} ${monedaEquivalencia}
+            </article>`;
+        
+        datosElem.insertAdjacentHTML('beforeend', datoEnHtml);
+    });
+}
+
+const submitForm = (evento) => {
+    // esto evita que la página se recargue
+    evento.preventDefault();
+
+    // Obtenemos los datos de la forma
+    const forma = document.forms.forma;
+
+    // de la forma, obtenemos el input
+    const inputEquivalencia = forma.elements.equivalencia;
+
+    // del input, obtenemos su valor...
+    const valorInput = inputEquivalencia.value;
+
+    // y ahora le damos ese valor a la variable global
+    monedaEquivalencia = valorInput;
+}
